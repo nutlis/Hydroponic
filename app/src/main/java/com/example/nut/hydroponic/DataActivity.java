ba898e4eec10d05ef0bbee19829be93b3d3b1fc9
nutlis <NutthapholKongrodj@gmail.com>
2017-12-24 23:15:34 +0700

    finish

--- app/src/main/java/com/example/nut/hydroponic/DataActivity.java
+++ app/src/main/java/com/example/nut/hydroponic/DataActivity.java
@@ -40,9 +40,7 @@ public class DataActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_data);
--- app/src/main/java/com/example/nut/hydroponic/MainActivity.java
+++ app/src/main/java/com/example/nut/hydroponic/MainActivity.java
@@ -30,9 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
         ImageView stats = (ImageView) findViewById(R.id.imageviewstats);
         stats.setOnClickListener(new View.OnClickListener() {
